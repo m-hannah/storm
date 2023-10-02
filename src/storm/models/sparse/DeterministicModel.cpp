839c8faff579330a4852f4278f426f428b659fa3
m-hannah <hannah.mertens@rwth-aachen.de>
2023-10-02 10:51:43 +0200

    print model information via --statistics

--- src/storm/models/sparse/DeterministicModel.cpp
+++ src/storm/models/sparse/DeterministicModel.cpp
@@ -10,131 +10,139 @@
 #include "utility/graph.h"
 
 namespace storm {
-namespace models {
-namespace sparse {
+    namespace models {
+        namespace sparse {
 
-template<typename ValueType, typename RewardModelType>
-DeterministicModel<ValueType, RewardModelType>::DeterministicModel(ModelType modelType,
-                                                                   storm::storage::sparse::ModelComponents<ValueType, RewardModelType> const& components)
-    : Model<ValueType, RewardModelType>(modelType, components) {
-    // Intentionally left empty
-}
+            template<typename ValueType, typename RewardModelType>
+            DeterministicModel<ValueType, RewardModelType>::DeterministicModel(ModelType modelType,
+                                                                               storm::storage::sparse::ModelComponents<ValueType, RewardModelType> const& components)
+                    : Model<ValueType, RewardModelType>(modelType, components) {
+                // Intentionally left empty
+            }
 
-template<typename ValueType, typename RewardModelType>
-DeterministicModel<ValueType, RewardModelType>::DeterministicModel(ModelType modelType,
-                                                                   storm::storage::sparse::ModelComponents<ValueType, RewardModelType>&& components)
-    : Model<ValueType, RewardModelType>(modelType, std::move(components)) {
-    // Intentionally left empty
-}
+            template<typename ValueType, typename RewardModelType>
+            DeterministicModel<ValueType, RewardModelType>::DeterministicModel(ModelType modelType,
+                                                                               storm::storage::sparse::ModelComponents<ValueType, RewardModelType>&& components)
+                    : Model<ValueType, RewardModelType>(modelType, std::move(components)) {
+                // Intentionally left empty
+            }
 
-template<typename ValueType, typename RewardModelType>
-void DeterministicModel<ValueType, RewardModelType>::writeDotToStream(std::ostream& outStream, size_t maxWidthLabel, bool includeLabeling,
-                                                                      storm::storage::BitVector const* subsystem, std::vector<ValueType> const* firstValue,
-                                                                      std::vector<ValueType> const* secondValue,
-                                                                      std::vector<uint_fast64_t> const* stateColoring, std::vector<std::string> const* colors,
-                                                                      std::vector<uint_fast64_t>* scheduler, bool finalizeOutput) const {
-    Model<ValueType, RewardModelType>::writeDotToStream(outStream, maxWidthLabel, includeLabeling, subsystem, firstValue, secondValue, stateColoring, colors,
-                                                        scheduler, false);
+            template<typename ValueType, typename RewardModelType>
+            void DeterministicModel<ValueType, RewardModelType>::writeDotToStream(std::ostream& outStream, size_t maxWidthLabel, bool includeLabeling,
+                                                                                  storm::storage::BitVector const* subsystem, std::vector<ValueType> const* firstValue,
+                                                                                  std::vector<ValueType> const* secondValue,
+                                                                                  std::vector<uint_fast64_t> const* stateColoring, std::vector<std::string> const* colors,
+                                                                                  std::vector<uint_fast64_t>* scheduler, bool finalizeOutput) const {
+                Model<ValueType, RewardModelType>::writeDotToStream(outStream, maxWidthLabel, includeLabeling, subsystem, firstValue, secondValue, stateColoring, colors,
+                                                                    scheduler, false);
 
-    // iterate over all transitions and draw the arrows with probability information attached.
-    auto rowIt = this->getTransitionMatrix().begin();
-    for (uint_fast64_t i = 0; i < this->getTransitionMatrix().getRowCount(); ++i, ++rowIt) {
-        // Put in an intermediate node if there is a choice labeling
-        std::string arrowOrigin = std::to_string(i);
-        if (this->hasChoiceLabeling()) {
-            arrowOrigin = "\"" + arrowOrigin + "c\"";
-            outStream << "\t" << arrowOrigin << " [shape = \"point\"]\n";
-            outStream << "\t" << i << " -> " << arrowOrigin << " [label= \"{";
-            storm::utility::outputFixedWidth(outStream, this->getChoiceLabeling().getLabelsOfChoice(i), maxWidthLabel);
-            outStream << "}\"];\n";
-        }
+                // iterate over all transitions and draw the arrows with probability information attached.
+                auto rowIt = this->getTransitionMatrix().begin();
+                for (uint_fast64_t i = 0; i < this->getTransitionMatrix().getRowCount(); ++i, ++rowIt) {
+                    // Put in an intermediate node if there is a choice labeling
+                    std::string arrowOrigin = std::to_string(i);
+                    if (this->hasChoiceLabeling()) {
+                        arrowOrigin = "\"" + arrowOrigin + "c\"";
+                        outStream << "\t" << arrowOrigin << " [shape = \"point\"]\n";
+                        outStream << "\t" << i << " -> " << arrowOrigin << " [label= \"{";
+                        storm::utility::outputFixedWidth(outStream, this->getChoiceLabeling().getLabelsOfChoice(i), maxWidthLabel);
+                        outStream << "}\"];\n";
+                    }
 
-        typename storm::storage::SparseMatrix<ValueType>::const_rows row = this->getTransitionMatrix().getRow(i);
-        for (auto const& transition : row) {
-            if (transition.getValue() != storm::utility::zero<ValueType>()) {
-                if (subsystem == nullptr || subsystem->get(transition.getColumn())) {
-                    outStream << "\t" << arrowOrigin << " -> " << transition.getColumn() << " [ label= \"" << transition.getValue() << "\" ];\n";
+                    typename storm::storage::SparseMatrix<ValueType>::const_rows row = this->getTransitionMatrix().getRow(i);
+                    for (auto const& transition : row) {
+                        if (transition.getValue() != storm::utility::zero<ValueType>()) {
+                            if (subsystem == nullptr || subsystem->get(transition.getColumn())) {
+                                outStream << "\t" << arrowOrigin << " -> " << transition.getColumn() << " [ label= \"" << transition.getValue() << "\" ];\n";
+                            }
+                        }
+                    }
+                }
+
+                if (finalizeOutput) {
+                    outStream << "}\n";
                 }
             }
-        }
-    }
 
-    if (finalizeOutput) {
-        outStream << "}\n";
-    }
-}
+            template<typename ValueType, typename RewardModelType>
+            void DeterministicModel<ValueType, RewardModelType>::printModelInformationToStream(std::ostream& out) const {
+                this->printModelInformationHeaderToStream(out);
+                this->printModelInformationFooterToStream(out);
 
-template<typename ValueType, typename RewardModelType>
-void DeterministicModel<ValueType, RewardModelType>::printModelInformationToStream(std::ostream& out) const {
-    this->printModelInformationHeaderToStream(out);
-    this->printModelInformationFooterToStream(out);
+                // TODO (only used for EVTs and steady state benchmarking), move to model-handling.h?
+                if (storm::settings::getModule<storm::settings::modules::CoreSettings>().isShowStatisticsSet()) {
+                    // Compute information about the model's topology:
+                    // Create auxiliary data and lambdas
+                    storm::storage::BitVector sccAsBitVector(this->getNumberOfStates(), false);
+                    auto isLeavingTransition = [&sccAsBitVector](auto const &e) { return !sccAsBitVector.get(e.getColumn()); };
+                    auto isExitState = [this, &isLeavingTransition](uint64_t state) {
+                        auto row = this->getTransitionMatrix().getRow(state);
+                        return std::any_of(row.begin(), row.end(), isLeavingTransition);
+                    };
 
-    // TODO (only used for EVTs and steady state benchmarking), move to model-handling.h?
-    if (storm::settings::getModule<storm::settings::modules::CoreSettings>().isShowStatisticsSet()) {
-        // Compute information about the model's topology:
-        // Create auxiliary data and lambdas
-        storm::storage::BitVector sccAsBitVector(this->getNumberOfStates(), false);
-        auto isLeavingTransition = [&sccAsBitVector](auto const &e) { return !sccAsBitVector.get(e.getColumn()); };
-        auto isExitState = [this, &isLeavingTransition](uint64_t state) {
-            auto row = this->getTransitionMatrix().getRow(state);
-            return std::any_of(row.begin(), row.end(), isLeavingTransition);
-        };
+                    auto options = storm::storage::StronglyConnectedComponentDecompositionOptions().forceTopologicalSort().computeSccDepths(
+                            true);
+                    auto sccDecomposition = storm::storage::StronglyConnectedComponentDecomposition<ValueType>(
+                            this->getTransitionMatrix(), options);
 
-        auto options = storm::storage::StronglyConnectedComponentDecompositionOptions().forceTopologicalSort().computeSccDepths(
-                true);
-        auto sccDecomposition = storm::storage::StronglyConnectedComponentDecomposition<ValueType>(
-                this->getTransitionMatrix(), options);
+                    storm::storage::BitVector nonBsccStates = storm::storage::BitVector(this->getNumberOfStates(), false);
+                    uint64_t numSccs = 0;
+                    uint64_t numBsccs = 0;
+                    uint64_t maxSccSize = 0;
+                    uint64_t maxBsccSize = 0;
+                    bool acyclic = true;
+                    for (auto const &scc: sccDecomposition) {
+                        sccAsBitVector.set(scc.begin(), scc.end(), true);
+                        uint64_t sccSize = sccAsBitVector.getNumberOfSetBits();
+                        if (std::any_of(sccAsBitVector.begin(), sccAsBitVector.end(), isExitState)) {
+                            // This is not a BSCC, mark the states correspondingly.
+                            nonBsccStates = nonBsccStates | sccAsBitVector;
+                            // Increase counter for number of non-bottom SCCs
+                            numSccs++;
+                            // Update the maximal number of states in one SCC
+                            maxSccSize = std::max(maxSccSize, sccSize);
+                            if (sccSize > 1) { // consider models with SCCs > 1 as acyclic
+                                acyclic = false;
+                            }
+                        } else {
+                            // Increase counter for number of non-bottom SCCs
+                            numBsccs++;
+                            // This is a BSCC, update the maximal number of states in one BSCC
+                            maxBsccSize = std::max(maxBsccSize, sccSize);
+                        }
+                        sccAsBitVector.clear();
+                    }
 
-        storm::storage::BitVector nonBsccStates = storm::storage::BitVector(this->getNumberOfStates(), false);
-        uint64_t numSccs = 0;
-        uint64_t maxSccSize = 0;
-        bool acyclic = true;
-        for (auto const &scc: sccDecomposition) {
-            sccAsBitVector.set(scc.begin(), scc.end(), true);
-            if (std::any_of(sccAsBitVector.begin(), sccAsBitVector.end(), isExitState)) {
-                // This is not a BSCC, mark the states correspondingly.
-                nonBsccStates = nonBsccStates | sccAsBitVector;
-                // Increase counter for number of non-bottom SCCs
-                numSccs++;
-                // Update the maximal number of states in one SCC
-                uint64_t sccSize = sccAsBitVector.getNumberOfSetBits();
-                maxSccSize = std::max(maxSccSize, sccSize);
-                if (sccSize > 1) { // consider models with SCCs > 1 as acyclic
-                    acyclic = false;
+                    out << "# Topology of the input model without BSCCs (acyclic = only non-bottom SCCs of size 1): ";
+                    if (acyclic) {
+                        out << "acyclic\n";
+                    } else {
+                        out << "cyclic\n";
+                    }
+
+                    out << "# Number of non-BSCC states: " << (nonBsccStates.getNumberOfSetBits()) << '\n';
+
+                    out << "# Number of non-bottom SCCs: " << numSccs << '\n';
+                    out << "# Number of BSCCs: " << numBsccs << '\n';
+                    out << "# Size of largest non-bottom SCC: " << maxSccSize << " states\n";
+                    out << "# Size of largest BSCC: " << maxBsccSize << " states\n";
+
+                    out << "# Length of max SCC chain: " << sccDecomposition.getMaxSccDepth() << '\n';
+
+                    out << "-------------------------------------------------------------- \n";
                 }
+
             }
-            sccAsBitVector.clear();
-        }
-
-        out << "# Topology of the input model without BSCCs (acyclic = only non-bottom SCCs of size 1): ";
-        if (acyclic) {
-            out << "acyclic\n";
-        } else {
-            out << "cyclic\n";
-        }
-
-        out << "# Number of non-BSCC states: " << (nonBsccStates.getNumberOfSetBits()) << '\n';
-
-        out << "# Number of non-bottom SCCs: " << numSccs << '\n';
-        out << "# Size of largest non-bottom SCC: " << maxSccSize << " states\n";
-
-
-        out << "# Length of max SCC chain: " << sccDecomposition.getMaxSccDepth() << '\n';
-
-        out << "-------------------------------------------------------------- \n";
-    }
-
-}
 
 
 
-template class DeterministicModel<double>;
+            template class DeterministicModel<double>;
 #ifdef STORM_HAVE_CARL
-template class DeterministicModel<storm::RationalNumber>;
+            template class DeterministicModel<storm::RationalNumber>;
 
-template class DeterministicModel<double, storm::models::sparse::StandardRewardModel<storm::Interval>>;
-template class DeterministicModel<storm::RationalFunction>;
+            template class DeterministicModel<double, storm::models::sparse::StandardRewardModel<storm::Interval>>;
+            template class DeterministicModel<storm::RationalFunction>;
 #endif
-}  // namespace sparse
-}  // namespace models
+        }  // namespace sparse
+    }  // namespace models
 }  // namespace storm
